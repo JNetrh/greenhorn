59294cb1dadf42665d9ce191c2958b69d5a6691a
JNetrh <netrh.j@gmail.com>
2018-10-16 23:42:29 +0200

    page wrapper & collors

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,5 +1,7 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
+// all settings:
+// https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin(
@@ -8,19 +10,22 @@ module.exports = function override(config, env) {
   );
   config = rewireLess.withLoaderOptions({
     modifyVars: { 
-      "@primary-color": "#1890ff",                         // primary color for all components
-      "@link-color": "#1890ff",                            // link color
-      "@success-color": "#52c41a",                         // success state color
-      "@warning-color": "#faad14",                         // warning state color
-      "@error-color": "#f5222d",                         // error state color
-      "@font-size - base": "14px",                           // major text font size
-      "@heading-color": "rgba(0, 0, 0, .85)",              // heading text color
-      "@text-color": "rgba(0, 0, 0, .65)",                 // major text color
-      "@text-color - secondary" : "rgba(0, 0, 0, .45)",      // secondary text color
-      "@disabled-color" : "rgba(0, 0, 0, .25)",            // disable state color
-      "@border-radius - base": "4px",                        // major border radius
-      "@border-color - base": "#d9d9d9",                     // major border color
-      "@box-shadow - base": "0 2px 8px rgba(0, 0, 0, .15)",  // major shadow for layers" 
+      "@primary-color": "#1890ff",                          // primary color for all components
+      "@link-color": "#1890ff",                             // link color
+      "@success-color": "#52c41a",                          // success state color
+      "@warning-color": "#faad14",                          // warning state color
+      "@error-color": "#f5222d",                            // error state color
+      "@font-size-base": "14px",                            // major text font size
+      "@heading-color": "rgba(0, 0, 0, .85)",               // heading text color
+      "@text-color": "rgba(0, 0, 0, .65)",                  // major text color
+      "@text-color-secondary" : "rgba(0, 0, 0, .45)",       // secondary text color
+      "@disabled-color" : "rgba(0, 0, 0, .25)",             // disable state color
+      "@border-radius-base": "4px",                         // major border radius
+      "@border-color-base": "#d9d9d9",                      // major border color
+      "@box-shadow-base": "0 2px 8px rgba(0, 0, 0, .15)",   // major shadow for layers" 
+      "@layout-body-background": "#f0f2f5",                 // color of body layout 
+      "@layout-header-background": "#001529",               // color of heather
+      "@layout-sider-background": "#1890ff",                // color of sider
     },
     javascriptEnabled: true,
   }) (config, env);
--- frontend/src/components/organisms/PageWrapper.js
+++ frontend/src/components/organisms/PageWrapper.js
@@ -3,12 +3,21 @@ import React from 'react';
 // import { Layout } from '../atoms/Layout';
 // import { PageFooter } from '../molecules/PageFooter';
 // import { TopNavBar } from '../molecules/TopNavBar';
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
+
 
 export const PageWrapper = ({ children }) => (
-  
+  <Layout>
+    <Header>Header</Header>
+    <Layout>
+      <Sider>Sider</Sider>
+      <Content>{children}</Content>
+    </Layout>
+    {/* <Footer>Footer</Footer> */}
+  </Layout>
   // <Layout className="container">
   //   <TopNavBar />
-  <div>{children}</div>
   //   <PageFooter />
   // </Layout>
 );
