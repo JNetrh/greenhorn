158010e3fe9fdfc9d04d01930276bf9625a75f5a
Rostislav Klein <rosta.klein@databreakers.com>
2018-10-18 00:26:12 +0200

    styled components integration idea

--- frontend/src/components/atoms/Logo.jsx
+++ frontend/src/components/atoms/Logo.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import './css/logo.css';
 import logo from '../../static/greenhorn_logo_dark.svg';
+import styled from 'styled-components';
+
+const LogoWrapper = styled.div`
+  width: 170px;
+  margin: -5px 20px 0 20px;
+  float: left;
+`;
 
 export default function Logo() {
   return (
-    <div className="logo">
+    <LogoWrapper>
       <img src={logo} alt="Greenhorn logo" />
-    </div>
+    </LogoWrapper>
   );
 }
