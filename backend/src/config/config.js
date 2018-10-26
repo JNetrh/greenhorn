bc5e3aa352dd0099e8c0524d01d3ffc313927d05
Rostislav Klein <rosta.klein@databreakers.com>
2018-10-26 22:28:25 +0200

    db config

---
+++ backend/src/config/config.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+};
