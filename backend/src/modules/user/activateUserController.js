fc64dd08fa4cc9df6d368e4ea67185b3df06e7fb
Rostislav Klein <rosta.klein@databreakers.com>
2018-10-28 23:03:47 +0100

    backend refactore user activate

--- backend/src/modules/user/activateUserController.js
+++ backend/src/modules/user/activateUserController.js
@@ -1,63 +1,51 @@
 import { Invitation, User } from '../../models';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import { stripPassword } from './addUserController';
 
-
-  export const stripPassword = user => {
-    const { password, ...rest } = user.toJSON();
-    return rest;
-  };
-
-  export const activateUserWithHashedPwd = async ({ password, ...user }) => {
-    const hashedPwd = await bcrypt.hash(password, 8);
-    const acivatedUser = await User.update({
-      ...user,
-      password: hashedPwd,
-      where: {
-        id: invitation.UserId,
-      },
-    });
-    return stripPassword(activatedUser);
-  };
-  
-  const activateUserController = async (req, res) => {   
+const activateUserController = async (req, res) => {
   const { password, passwordRepeat } = req.body;
+  console.log(password, passwordRepeat);
   const { token } = req.params;
-  const verified = jwt.verify(token, process.env.SECRET);
+  try {
+    const verified = jwt.verify(token, process.env.SECRET);
 
-  if (!verified) {
-    res.status(401).json({ msg: 'Token not valid' });
-  }
-  const invitation = await Invitation.findById(verified.invitationId, {
-    include: [User],
-  });
-  if (!invitation) {
-    res.status(404).json({ msg: 'This invitation does not exist.' });
-  }
-    try {
-      if (password !== passwordRepeat) {
-        return res
-          .status(400)
-          .json({ msg: 'Password and repeated password do not match.' });
-      }
-      else if (!password || !passwordRepeat) {
-        return res
-          .status(400)
-          .json({ msg: 'Please enter both password and password repeated' });
-      }
-        const hashedPwd = await bcrypt.hash(password, 8);
-        const activatedUser = await User.update({
-          password: hashedPwd,  
-        }, 
-        {where: {
-          id: invitation.UserId,
-        },}
-      );
-      return res.json('Bombs away');
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({ msg: 'Activate User internal Error ' });
+    if (!verified) {
+      res.status(401).json({ msg: 'Token not valid' });
     }
-  };
-  
-  export default activateUserController;
+    const invitation = await Invitation.findById(verified.invitationId, {
+      include: [User],
+    });
+    if (!invitation) {
+      res.status(404).json({ msg: 'This invitation does not exist.' });
+    }
+
+    if (password !== passwordRepeat) {
+      return res
+        .status(400)
+        .json({ msg: 'Password and repeated password do not match.' });
+    } else if (!password || !passwordRepeat) {
+      return res
+        .status(400)
+        .json({ msg: 'Please enter both password and password repeated' });
+    }
+    const hashedPwd = await bcrypt.hash(password, 8);
+    await User.update(
+      {
+        password: hashedPwd,
+      },
+      {
+        where: {
+          id: invitation.UserId,
+        },
+      }
+    );
+    const activatedUser = await User.findById(invitation.UserId);
+    return res.json(stripPassword(activatedUser));
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: 'Activate User internal Error ' });
+  }
+};
+
+export default activateUserController;
--- backend/src/modules/user/routes.js
+++ backend/src/modules/user/routes.js
@@ -5,14 +5,12 @@ import addUserController from './addUserController';
 import usersController from './usersController';
 import { getUserByInvitationToken } from './invitationController';
 import activateUserController from './activateUserController';
-import test from './test';
 
 const router = expressAsyncAwait(Router());
 router.get('/', usersController);
 router.post('/', addUserController);
 router.get('/byinvitation/:token', getUserByInvitationToken);
 
-router.put('/activate/:token',activateUserController);
-
+router.put('/activate/:token', activateUserController);
 
 export default router;
