cb8eea67244c5a018cbb92bc2a6234bbfde798da
pololitr <stepan.chmel@gmail.com>
2018-11-02 11:19:21 +0100

    addTasksController logic added

--- backend/src/models/Task.js
+++ backend/src/models/Task.js
@@ -3,10 +3,12 @@ module.exports = (sequelize, DataTypes) => {
   var Task = sequelize.define("Task", {
     title: DataTypes.STRING,
     estimatedTime: DataTypes.INTEGER,
-    severity: DataTypes.STRING
+    severity: DataTypes.STRING,
+    description: DataTypes.STRING
   });
 
-  Task.associate = ({ Task, AssignedTask, Group }) => {
+  Task.associate = ({ Task, AssignedTask, Group, User }) => {
+    User.hasMany(Task, { as: 'createdBy', foreignKey: 'createdBy' });
     Task.hasMany(AssignedTask);
     Group.hasMany(Task);
   };
