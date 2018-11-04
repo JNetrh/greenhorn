aeab277d60bbb7b820529f2bdf9a81b20516c923
JNetrh <netrh.j@gmail.com>
2018-11-05 00:23:54 +0100

    asigning users to groups & tasks to groups api

--- backend/src/modules/assign/task/assignTaskToGroup.js
+++ backend/src/modules/assign/task/assignTaskToGroup.js
@@ -1,1 +1,40 @@
-export const assignTaskToGroup = async (req, res) => {};
+import { Task, Group } from '../../../models';
+
+export const assignTaskToGroup = async (req, res) => {
+  const { groupId, taskIds } = req.body;
+
+  if (!Array.isArray(taskIds)) {
+    res.status(400).json({ msg: 'tasks has to be an array' });
+  }
+
+  const group = await Group.findByPk(groupId);
+
+  if (!group) {
+    res.status(400).json({ msg: 'group not found' });
+  }
+
+  const tasks = await Task.findAll({
+    where: {
+      id: taskIds,
+    },
+  });
+
+  if (!tasks) {
+    res.status(400).json({ msg: 'error fetching tasks' });
+  }
+
+  await Task.update(
+    {
+      GroupId: group.id,
+    },
+    {
+      where: {
+        id: taskIds,
+      },
+    }
+  );
+
+  const groupTasks = await Group.findByPk(groupId, { include: [Task] });
+
+  res.json(groupTasks);
+};
--- backend/src/modules/assign/user/assignUserToGroup.js
+++ backend/src/modules/assign/user/assignUserToGroup.js
@@ -1,13 +1,4 @@
 import { User, Group } from '../../../models';
-import { read } from 'fs';
-
-const setUserToGroup = async (user, groups) => {
-  console.log(Group.prototype);
-  // return Object.keys(User.Instance.prototype);
-  // console.log('groups:', groups);
-  // return await User.prototype.setGroups(groups);
-  return await groups[0].addUser(user);
-};
 
 const findGroupsById = async groupIds => {
   return await Group.findAll({
@@ -19,13 +10,12 @@ const findGroupsById = async groupIds => {
 
 export const assignUserToGroup = async (req, res) => {
   const { userId, groupIds } = req.body;
-  console.log('groupIDs: ', groupIds);
 
   if (!Array.isArray(groupIds)) {
     return res.status(400).json({ msg: 'group ids needs to be an array' });
   }
 
-  const user = User.findByPk(userId);
+  const user = await User.findByPk(userId);
 
   if (!user) {
     return res.status(400).json({ msg: 'User not found' });
@@ -33,7 +23,15 @@ export const assignUserToGroup = async (req, res) => {
 
   const groups = await findGroupsById(groupIds);
 
-  const assignedGroups = await setUserToGroup(user, groupIds);
+  if (!groups) {
+    return res.status(400).json({ msg: 'Group not found' });
+  }
+
+  await user.addGroups(groups);
+
+  const assignedGroups = await User.findByPk(userId, {
+    include: [Group],
+  });
 
   res.json(assignedGroups);
 };
