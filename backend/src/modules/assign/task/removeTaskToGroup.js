83a681c29e032383d84d6e675b243e13c234c19f
JNetrh <netrh.j@gmail.com>
2018-11-05 12:19:59 +0100

    changes in return values and try/catch block added to assign api

--- backend/src/modules/assign/task/removeTaskToGroup.js
+++ backend/src/modules/assign/task/removeTaskToGroup.js
@@ -3,25 +3,29 @@ import { Task, Group } from '../../../models';
 export const removeTaskToGroup = async (req, res) => {
   const { groupId, taskIds } = req.body;
 
-  if (!Array.isArray(taskIds)) {
-    return res.status(400).json({ msg: 'tasks has to an array' });
+  try {
+    if (!Array.isArray(taskIds)) {
+      return res.status(400).json({ msg: 'tasks has to an array' });
+    }
+
+    const group = await Group.findByPk(groupId);
+
+    if (!group) {
+      return res.status(404).json({ msg: 'group not found' });
+    }
+
+    const tasks = await Task.findAll({ where: { id: taskIds } });
+
+    if (!tasks) {
+      return res.status(400).json({ msg: 'task fetch failed' });
+    }
+
+    await group.removeTasks(tasks);
+
+    const groupTasks = await Group.findByPk(groupId, { include: [Task] });
+
+    return res.json(groupTasks);
+  } catch (error) {
+    return res.status(500).json(error);
   }
-
-  const group = await Group.findByPk(groupId);
-
-  if (!group) {
-    return res.status(400).json({ msg: 'group not found' });
-  }
-
-  const tasks = await Task.findAll({ where: { id: taskIds } });
-
-  if (!tasks) {
-    return res.status(400).json({ msg: 'task fetch failed' });
-  }
-
-  await group.removeTasks(tasks);
-
-  const groupTasks = await Group.findByPk(groupId, { include: [Task] });
-
-  return res.json(groupTasks);
 };
--- backend/src/modules/assign/user/assignUserToGroup.js
+++ backend/src/modules/assign/user/assignUserToGroup.js
@@ -11,27 +11,35 @@ const findGroupsById = async groupIds => {
 export const assignUserToGroup = async (req, res) => {
   const { userId, groupIds } = req.body;
 
-  if (!Array.isArray(groupIds)) {
-    return res.status(400).json({ msg: 'group ids needs to be an array' });
+  try {
+    if (!Array.isArray(groupIds)) {
+      return res.status(400).json({ msg: 'group ids needs to be an array' });
+    }
+
+    const user = await User.findByPk(userId);
+
+    if (!user) {
+      return res.status(404).json({ msg: 'User not found' });
+    }
+
+    const groups = await findGroupsById(groupIds);
+
+    if (!groups) {
+      return res.status(400).json({ msg: 'Groups fetch failed' });
+    }
+
+    await user.addGroups(groups);
+
+    const assignedGroups = await User.findByPk(userId, {
+      include: [Group],
+    });
+
+    return res.json(assignedGroups);
+  } catch (error) {
+    return res.status(500).json(error);
   }
-
-  const user = await User.findByPk(userId);
-
-  if (!user) {
-    return res.status(400).json({ msg: 'User not found' });
-  }
-
-  const groups = await findGroupsById(groupIds);
-
-  if (!groups) {
-    return res.status(400).json({ msg: 'Group not found' });
-  }
-
-  await user.addGroups(groups);
-
-  const assignedGroups = await User.findByPk(userId, {
-    include: [Group],
-  });
-
-  return res.json(assignedGroups);
 };
+
+// -- HELPERS -- //
+
+const asignTasksFromGroups = async (user, groups) => {};
