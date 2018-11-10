add3fc7dfba16ae23d4b5be49e779867671dffa0
Jakub Netrh <netrh.j@gmail.com>
2018-11-10 20:45:18 +0100

    Display assigned tasks to user

--- backend/src/modules/assignedTask/listAssignedTasks.js
+++ backend/src/modules/assignedTask/listAssignedTasks.js
@@ -1,13 +1,5 @@
 import { AssignedTask, Task } from '../../models';
 export const listAssignedTasks = async (req, res) => {
-  // const assignedTask = await Task.findAll({
-  //   include: [
-  //     {
-  //       model: AssignedTask,
-  //       where: { UserId: req.userId },
-  //     },
-  //   ],
-  // });
   const assignedTask = await AssignedTask.findAll({
     where: { UserId: req.userId },
     include: [Task],
--- frontend/src/components/pages/HomePage/AssignedTasks.jsx
+++ frontend/src/components/pages/HomePage/AssignedTasks.jsx
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'antd';
+import Table from '../../molecules/Table';
+import { getLongDate } from '../../../helpers/dateFormat';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'Task.title',
+    key: 'name',
+  },
+  {
+    title: 'description',
+    dataIndex: 'Task.description',
+    key: 'description',
+    width: 150,
+  },
+  {
+    title: 'Severity',
+    dataIndex: 'Task.severity',
+    key: 'severity',
+  },
+  {
+    title: 'Submit untill',
+    dataIndex: 'untill',
+    key: 'untill',
+    render: date => getLongDate(date),
+  },
+];
 
 export class AssignedTasks extends Component {
-  componentDidMount() {
-    const { startListAssignedTasks } = this.props;
-    startListAssignedTasks();
-  }
+  componentDidMount = () => {
+    const { startListAssignedTasks, fetched } = this.props;
+    !fetched && startListAssignedTasks();
+  };
+
   render() {
-    console.log(this.props);
+    const { assignedTasks, isLoading } = this.props;
     return (
-      <Row>
-        <Col>{'Hi'}</Col>
-      </Row>
+      <Table
+        loading={isLoading}
+        columns={columns}
+        dataSource={assignedTasks.map((task, i) => {
+          task['key'] = i;
+          return task;
+        })}
+        rowLink={({ id }) => `/task/${id}`}
+      />
     );
   }
 }
