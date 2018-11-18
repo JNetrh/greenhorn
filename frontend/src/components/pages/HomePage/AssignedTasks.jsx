30d14ee8f071346adde26009414ca0d83da21d3e
Jakub Netrh <netrh.j@gmail.com>
2018-11-18 12:38:52 +0100

    assigned tasks display

--- frontend/src/components/pages/HomePage/AssignedTasks.jsx
+++ frontend/src/components/pages/HomePage/AssignedTasks.jsx
@@ -1,31 +1,25 @@
 import React, { Component } from 'react';
-import Table from '../../molecules/Table';
-import { getLongDate } from '../../../helpers/dateFormat';
+import { List, Icon, Row, Col } from 'antd';
+import styled from 'styled-components';
+import { TaskItem } from '../../organisms/TaskItem';
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'Task.title',
-    key: 'name',
-  },
-  {
-    title: 'description',
-    dataIndex: 'Task.description',
-    key: 'description',
-    width: 150,
-  },
-  {
-    title: 'Severity',
-    dataIndex: 'Task.severity',
-    key: 'severity',
-  },
-  {
-    title: 'Submit untill',
-    dataIndex: 'untill',
-    key: 'untill',
-    render: date => getLongDate(date),
-  },
-];
+const Wrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+// const listData = [];
+// for (let i = 0; i < 23; i++) {
+//   listData.push({
+//     href: 'http://ant.design',
+//     title: `ant design part ${i}`,
+//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+//     description:
+//       'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+//     content:
+//       'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+//   });
+// }
 
 export class AssignedTasks extends Component {
   componentDidMount = () => {
@@ -34,17 +28,26 @@ export class AssignedTasks extends Component {
   };
 
   render() {
-    const { assignedTasks, isLoading } = this.props;
+    const { assignedTasks, isLoading, ...rest } = this.props;
     return (
-      <Table
-        loading={isLoading}
-        columns={columns}
-        dataSource={assignedTasks.map((task, i) => {
-          task['key'] = i;
-          return task;
-        })}
-        rowLink={({ id }) => `/submit/${id}`}
-      />
+      <Wrapper>
+        <Row>
+          <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+            <List
+              itemLayout="vertical"
+              size="large"
+              pagination={{
+                onChange: page => {
+                  console.log(page);
+                },
+                pageSize: 3,
+              }}
+              dataSource={assignedTasks}
+              renderItem={item => <TaskItem item={item} {...rest} />}
+            />
+          </Col>
+        </Row>
+      </Wrapper>
     );
   }
 }
