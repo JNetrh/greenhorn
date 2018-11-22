9507e507c28ac11bfa4e904e3c4e33d592a4f7fa
Rostislav Klein <rosta.klein@databreakers.com>
2018-11-22 21:55:21 +0100

    assigning groups on create

--- backend/src/modules/user/addUserController.js
+++ backend/src/modules/user/addUserController.js
@@ -3,6 +3,7 @@ import { createInvitation } from '../../services/invitation/invitationController
 import bcrypt from 'bcryptjs';
 import { stripPassword } from '../../services/password/stripPassword';
 import { addUserMail } from '../../services/mail/addUserMail';
+import { updateUserGroupsAndAssignTasks } from '../assign/assignUserToGroup';
 // import { updateUserGroupsAndAssignTasks } from '../assign/assignUserToGroup';
 
 export const ROLES = ['user', 'taskowner', 'hr'];
@@ -15,7 +16,7 @@ export const getUserByEmail = async email => {
 
 export const createUserWithHashedPwd = async ({
   password,
-  // groups,
+  groups,
   ...user
 }) => {
   const hashedPwd = await bcrypt.hash(password, 8);
@@ -24,13 +25,14 @@ export const createUserWithHashedPwd = async ({
     password: hashedPwd,
     role: user.role || ROLES[0],
   });
-  // await updateUserGroupsAndAssignTasks(createdUser.id, groups);
-  return stripPassword(createdUser);
+  await updateUserGroupsAndAssignTasks(createdUser.id, groups);
+  const userWithGroups = await User.findById(createdUser.id);
+  return stripPassword(userWithGroups);
 };
 
 export const addUser = async user => {
   return new Promise(async (resolve, reject) => {
-    const { name, surname, email, password } = user.body;
+    const { name, surname, groups, email, password } = user.body;
     try {
       if (!name || !surname || !email || !password) {
         reject({
@@ -51,21 +53,20 @@ export const addUser = async user => {
         surname,
         email,
         password,
+        groups,
       });
       try {
-        const invitationWithToken = await createInvitation(createdUser.id);
-        console.log('invitationWithToken ', invitationWithToken);
-
-        const invitationSent = addUserMail(createdUser, invitationWithToken);
+        const token = await createInvitation(createdUser.id);
+        addUserMail(createdUser, token);
       } catch (err) {
         console.log(err);
-        reject({ error, status: 500 });
+        reject({ error: 'Could not send invitation', status: 500 });
       }
 
       resolve(createdUser);
     } catch (err) {
       console.log(err);
-      reject({ error, status: 500 });
+      reject({ error: 'Could not create user', status: 500 });
     }
   });
 };
