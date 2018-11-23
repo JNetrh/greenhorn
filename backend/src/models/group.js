9c3df8f66e80364aadd86e579531f6069461c1d6
Rostislav Klein <rosta.klein@databreakers.com>
2018-11-23 13:24:14 +0100

    varchar limit char length fixed

--- backend/src/models/group.js
+++ backend/src/models/group.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Group = sequelize.define('Group', {
     name: DataTypes.STRING,
-    description: DataTypes.STRING,
+    description: DataTypes.TEXT,
   });
   return Group;
 };
