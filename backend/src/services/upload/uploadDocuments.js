6198e47d02ecc00a3f84f714d15733bd136a1611
Rostislav Klein <rosta.klein@databreakers.com>
2018-12-09 15:47:23 +0100

    es6 refactor

--- backend/src/services/upload/uploadDocuments.js
+++ backend/src/services/upload/uploadDocuments.js
@@ -1,28 +1,25 @@
 import multer from 'multer';
-import path from 'path';
-var express = require('express')
-var aws = require('aws-sdk')
-var stream = require('stream');
-var multerS3 = require('multer-s3')
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3');
 
-var s3 = new aws.S3({
+const s3 = new aws.S3({
   AWS_ACCESS_KEY: 'AKIAJGNKVNZQOUDRDLNA',
-	AWS_SECRET_ACCESS_KEY: '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',
-  REGION : 'eu-central-1',
+  AWS_SECRET_ACCESS_KEY: 'ES3iwNLc/LDZ9ZECPg6X2qVshm4i9UEnQNu9cQGC',
+  REGION: 'eu-central-1',
   Bucket: 't9greenhorn',
-})
+});
 
-var doUpload =multer({
-  storage:multerS3({
-    s3: s3,
+const doUpload = multer({
+  storage: multerS3({
+    s3,
     bucket: 't9greenhorn',
-    metadata: function (req, file, cb) {
-      cb(null, {fieldName: file.fieldname});
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
     },
-    key: function (req, file, cb){
-      cb(null, Date.now().toString())
-    }
-  })
-})
- 
+    key: (req, file, cb) => {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
+
 export const upload = multer({ doUpload }).array('documents');
