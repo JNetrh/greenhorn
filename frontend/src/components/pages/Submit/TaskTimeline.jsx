ffcd6f59345b4394af250f66d11ac91742195b41
Kaja <kohlovakarolina@seznam.cz>
2018-12-28 02:18:59 +0100

    workflow upgraded

--- frontend/src/components/pages/Submit/TaskTimeline.jsx
+++ frontend/src/components/pages/Submit/TaskTimeline.jsx
@@ -2,19 +2,45 @@ import React from 'react';
 import { Timeline } from 'antd';
 import { getLongDate } from '../../../helpers/dateFormat';
 import { getWorkflowColor } from '../../../helpers/getColor';
+import moment from 'moment';
 
 export const TaskTimeline = ({ workflow }) => (
-  <Timeline style={{ marginTop: 25 }}>
-    {workflow.map(item => (
-      <Timeline.Item
-        key={item.id}
-        color={getWorkflowColor(item.TaskStatus.name)}
-      >
-        {getLongDate(item.createdAt)} - {getItemText(item)}
-      </Timeline.Item>
-    ))}
-  </Timeline>
+  workflow.sort(compare),
+  (
+    <Timeline style={{ marginTop: 25 }}>
+      {workflow.map(item => (
+        <Timeline.Item
+          key={item.id}
+          color={getWorkflowColor(item.TaskStatus.name)}
+        >
+          {moment(item.createdAt).format('D. MMM YYYY HH:mm')} -{' '}
+          {getItemText(item)}.
+          <li>
+            {item.note
+              ? ' Commented by ' +
+                item.submittedBy.name +
+                ' ' +
+                item.submittedBy.surname +
+                ': ' +
+                item.note
+              : ''}
+          </li>
+          <li>
+            {item.Documents.length > 0
+              ? ' Uploaded File: ' + item.Documents[0].name
+              : ''}
+          </li>
+        </Timeline.Item>
+      ))}
+    </Timeline>
+  )
 );
+
+function compare(a, b) {
+  if (a.createdAt > b.createdAt) return -1;
+  if (a.createdAt < b.createdAt) return 1;
+  return 0;
+}
 
 const getItemText = item => {
   switch (item.TaskStatus.name) {
